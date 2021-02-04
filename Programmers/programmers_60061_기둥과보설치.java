2afa5fd5d64c86ba4ccacf7e8021f2785a352ba9
good797979 <good79797979@naver.com>
2021-02-05 00:28:08 +0900

    프로그래머스 60061 기둥과보설치 완료

--- Programmers/programmers_60061_기둥과보설치.java
+++ Programmers/programmers_60061_기둥과보설치.java
@@ -2,105 +2,94 @@
 
 import java.io.*;
 
-public class Main {
-    static int[][] map;
-    static int n;
-
-    public static void main(String[] args) throws IOException {
-        int[][] arr = {
-                {1, 0, 0, 1},
-                {1, 1, 1, 1},
-                {2, 1, 0, 1},
-                {2, 2, 1, 1},
-                {5, 0, 0, 1},
-                {5, 1, 0, 1},
-                {4, 2, 1, 1},
-                {3, 2, 1, 1}
-        };
-
-        int n = 5;
-        map = new int[n][n];
-
-        int[][] ans = solution(n, arr);
-
-
-    }
-
+class Solution {
+    static boolean[][] pillars, beams;
+    static int count = 0;
+    static final int PILLAR = 0;
+    static final int BEAM = 1;
+    static final int DESTRUCT = 0;
+    static final int CONSTRUCT = 1;
+    
     public static int[][] solution(int n, int[][] build_frame) {
         int[][] answer = {};
+        pillars = new boolean[n+3][n+3];
+        beams = new boolean[n+3][n+3];
 
+        for(int i=0;i<n+3;i++){
+            for(int j=0;j<n+3;j++){
+                pillars[i][j]=false;
+                beams[i][j]=false;
+            }
+        }
 
         for (int i = 0; i < build_frame.length; i++) {
-            int x = build_frame[i][0];
-            int y = build_frame[i][1];
-            int a = build_frame[i][2];//기둥인지 보인지
-            int b = build_frame[i][3];//설치할지 삭제할지
+            int x = build_frame[i][0]+1;
+            int y = build_frame[i][1]+1;
+            int shape = build_frame[i][2];//기둥인지 보인지
+            int command = build_frame[i][3];//설치할지 삭제할지
 
-            make(x, y, a, b);
-            //-1은 보, 1은 기둥
+            make(x, y, shape, command, n);
+        }
+
+        answer = new int[count][3];
+        int k=0;
+        for(int i=1;i<=n+1;i++){
+            for(int j=1;j<=n+1;j++){
+                if(pillars[i][j])
+                    answer[k++] = new int[]{i-1, j-1, PILLAR};
+                if(beams[i][j])
+                    answer[k++] = new int[]{i-1, j-1, BEAM};
+            }
         }
 
         return answer;
     }
 
-    public static void make(int x, int y, int a, int b)
-    {
-        if (a == 0) {//기둥
-            if (checkGidoong(y, x)) {
-                if (b == 0) {
-                    map[y][x] = 1;
-                    map[y + 1][x] = 1;
-                } else {
-                    if (map[y + 1][x]==-1){
-                        if(map[y+1][x+1]==1){
-                            if(inBound(y,x+1) && map[y][x+1]==1)
-                        }
-                    }
+    public static void make(int x, int y, int shape, int command, int n) {
+        if(command==CONSTRUCT){
+            if(shape==PILLAR){
+                if(constructPillar(x, y)){
+                    pillars[x][y]=true;
+                    count++;
+                }
+            }else{
+                if(constructBeam(x, y)){
+                    beams[x][y]=true;
+                    count++;
                 }
             }
-        } else {//보
-            if (checkBo(y, x)) {
-                if(b==0){
-                    map[y][x]=-1;
-                    map[y][x+1]=-1;
-                }else{
-                    if(map[y][x+1]==1){
-                        if(inBound(y+1, x+1) && map[y+1][x+1]==1){
-                            return;
-                        }else{
-                            if(inBound(y-1, x) && map[y-1][x]==)
-                                map[y][x]=0;
-                        }
-                    }else if(map[y][x+1]==-1){
+        }else{//command==DESTRUCT
+            if(shape==PILLAR){
+                pillars[x][y]=false;
+            }else{
+                beams[x][y]=false;
+            }
+            count--;
 
-                    }else{
+            if(!destruct(n)){
+                count++;
+                if(shape==PILLAR) pillars[x][y]=true;
+                if(shape==BEAM) beams[x][y]=true;
+            }
 
-                    }
-                }
+        }
+    }
+
+    public static boolean constructPillar(int x, int y){
+        return y==1 || pillars[x][y-1] || beams[x][y] || beams[x-1][y];
+    }
+
+    public static boolean constructBeam(int x, int y){
+        return pillars[x][y-1] || pillars[x+1][y-1] || (beams[x-1][y] && beams[x+1][y]);
+    }
+
+    public static boolean destruct(int n){
+        for(int i=1;i<=n+1;i++){
+            for(int j=1;j<=n+1;j++){
+                if(pillars[i][j] && !constructPillar(i, j)) return false;
+                if(beams[i][j] && !constructBeam(i, j)) return false;
             }
         }
-    }
-
-    public static boolean checkGidoong(int y, int x) {
-        if (inBound(y, x) && inBound(y + 1, x)) {
-            if (y == 0 || map[y][x] != 0)
-                return true;
-            return false;
-        }
-        return false;
-    }
-
-    public static boolean checkBo(int y, int x) {
-        if (inBound(y, x) && inBound(y, x + 1)) {
-            if (map[y][x] == 1 || map[y][x + 1] == 1 || (map[y][x] == -1 && map[y][x + 1] == -1))
-                return true;
-            return false;
-        }
-        return false;
-    }
-
-    public static boolean inBound(int y, int x) {
-        if (y >= 0 && x >= 0 && y <= n - 1 || x <= n - 1) return true;
-        return false;
+        return true;
     }
 }
